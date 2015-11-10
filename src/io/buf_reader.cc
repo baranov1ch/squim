89ddbca7b59107fec38859a02b9b6d356868a973
Alexey Baranov <baranovich@yandex-team.ru>
2015-11-10 16:54:14 +0300

    impl of some ctor/dtor

--- src/io/buf_reader.cc
+++ src/io/buf_reader.cc
@@ -1,3 +1,10 @@
 #include "io/buf_reader.h"
+#include "io/buffered_source.h"
 
-namespace io {}  // namespace io
+namespace io {
+
+BufReader::BufReader(std::unique_ptr<BufferedSource> source) : source_(std::move(source)) {}
+
+BufReader::~BufReader() {}
+
+}  // namespace io
