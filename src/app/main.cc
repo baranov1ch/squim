7a22a71ad34b60d7830724904e1640ebab08c2ca
Alexey Baranov <baranovich@yandex-team.ru>
2015-11-15 17:54:11 +0300

    Glog and Gflags ready

--- src/app/main.cc
+++ src/app/main.cc
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <sys/utsname.h>
+#include <unwind.h>
+#include <syscall.h>
+#include <sys/syscall.h>
 
+#include "glog/logging.h"
 #include "webp/encode.h"
-#include "third_party/gflags/src/gflags.h"
+#include "gflags/gflags.h"
 
 DEFINE_bool(do_nothing, true, "If true, does nothing");
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  std::cout << "Hello, World! " << FLAGS_do_nothing << std::endl;
+  google::InitGoogleLogging(argv[0]);
+  LOG(INFO) << "Hello, World! " << FLAGS_do_nothing;
 	auto success = WebPEncode(nullptr, nullptr);
   if (!success)
     return -1;
