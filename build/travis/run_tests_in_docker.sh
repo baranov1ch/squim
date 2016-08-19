f6a84991fabf89e02acdb7f738fa7f8ab60a62fd
Alexey Baranov <me@kotiki.cc>
2016-08-19 16:44:25 +0300

    Move travis build files into separate folder

--- build/travis/run_tests_in_docker.sh
+++ build/travis/run_tests_in_docker.sh
@@ -14,35 +14,34 @@
 
 set -ex
 
-cd `dirname $0`/..
+cd `dirname $0`/../..
 git_root=`pwd`
 cd -
 
-DOCKER_IMAGE_NAME=squim_docker_`sha1sum ${git_root}/build/ci_docker/Dockerfile | cut -f1 -d\ `
+DOCKER_IMAGE_NAME=squim_docker_`sha1sum ${git_root}/build/travis/Dockerfile | cut -f1 -d\ `
 
 echo $DOCKER_IMAGE_NAME
 
-docker build -t $DOCKER_IMAGE_NAME ${git_root}/build/ci_docker
+docker build -t $DOCKER_IMAGE_NAME ${git_root}/build/travis
 
-CONTAINER_NAME="run_tests_$(uuidgen)"
+CONTAINER_NAME="travis_run_tests_$(uuidgen)"
 
 echo $CONTAINER_NAME
 
 opts="--verbose_failures  -c dbg"
-
 tests=`cat ${git_root}/build/testsuites | tr "\n" " "`
 
-RUN_TESTS_COMMAND="bazel test ${tests} ${opts}"
+COMMAND="bazel test ${tests} ${opts}"
 
-echo $RUN_TESTS_COMMAND
+echo $COMMAND
 
 docker run \
-  -e "RUN_TESTS_COMMAND=$RUN_TESTS_COMMAND" \
+  -e "COMMAND=$COMMAND" \
   -v "$git_root:/var/local/ci/squim" \
   -w /var/local/ci/squim/ \
   --name=$CONTAINER_NAME \
   $DOCKER_IMAGE_NAME \
-  bash -l /var/local/ci/squim/build/docker_run_tests.sh || DOCKER_FAILED="true"
+  bash -l ./build/travis/setup_bazel_and_run_tests.sh || DOCKER_FAILED="true"
 
 docker rm -f $CONTAINER_NAME || true
 
